9aa89345441a36ce3a612db360b290522918ec3f
Will Farhat <willf668@gmail.com>
2022-01-31 13:39:15 -0800

    header changes

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -7,14 +7,15 @@ export default function Banner() {
     return <div id="banner">
         {(window.location.pathname === "/")
             ? <div id="bannerName" className='dropShadow'>Dapz</div>
-            : <a id="bannerName" className='dropShadow' href={homeURL} rel='noreferrer' target={"_self"}>Dapz's Site</a>
+            : <a id="bannerName" className='dropShadow' href={homeURL} rel='noreferrer' target={"_self"}>Dapz</a>
         }
-        <div id="socials">
+        <div id="socials" className='dropShadow'>
             <SocialLink name="Twitter" link="https://twitter.com/TheDapuma" />
             <SocialLink name="Instagram" link="https://www.instagram.com/dapumadapz/" />
             <SocialLink name="Discord" link="https://discord.com/invite/h6kWeN74rb" />
             <SocialLink name="Twitch" link="https://www.twitch.tv/actuallydapz" />
-            <SocialLink name="YouTube" link="https://www.youtube.com/c/Dapz1" />
+            <SocialLink name="YouTube" link="https://www.youtube.com/c/Dapz1" className="socialLast" />
+            <SocialLink name="corner" />
         </div>
     </div>;
 }
--- src/Components/SocialLink.js
+++ src/Components/SocialLink.js
@@ -4,6 +4,10 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faInstagram, faTwitter, faYoutube, faDiscord, faTwitch } from '@fortawesome/free-brands-svg-icons'
 
 export default function SocialLink(props) {
+    let classes = "socialLink";
+    if ("className" in props) {
+        classes += " " + props.className;
+    }
     const linkMap = {
         "Instagram": faInstagram,
         "Twitter": faTwitter,
@@ -11,8 +15,12 @@ export default function SocialLink(props) {
         "Discord": faDiscord,
         "Twitch": faTwitch
     }
-
-    return <a className="socialLink" target="_blank" href={props.link} rel="noreferrer">
-        <FontAwesomeIcon icon={linkMap[props.name]} />
-    </a>;
+    if (props.name === "corner") {
+        return <div className="socialLink socialLinkCorner"></div>
+    }
+    else {
+        return <a className={classes} target="_blank" href={props.link} rel="noreferrer">
+            <FontAwesomeIcon icon={linkMap[props.name]} />
+        </a>;
+    }
 }
